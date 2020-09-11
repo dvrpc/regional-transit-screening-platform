0db5f84ae2ee2b6e4a32da0acedaaf4d62c37f25
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2020-09-11 15:04:36 -0400

    read the .env file into python variables

---
+++ regional_transit_screening_platform/__init__.py
@@ -0,0 +1,14 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
+
+from .database import PostgreSQL
+
+load_dotenv(find_dotenv())
+DB_USER = os.getenv("DB_USER")
+DB_PW = os.getenv("DB_PW")
+SQL_DB_NAME = os.getenv("SQL_DB_NAME")
+GDRIVE_PROJECT_FOLDER = os.getenv("GDRIVE_PROJECT_FOLDER")
+
+db = PostgreSQL(SQL_DB_NAME)
+file_root = Path(GDRIVE_PROJECT_FOLDER)
