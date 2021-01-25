96d502a77da55534c67f5da878de670720ae8de2
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2021-01-25 17:43:22 -0500

    add NJT command

--- regional_transit_screening_platform/step_05_ridership/cmd.py
+++ regional_transit_screening_platform/step_05_ridership/cmd.py
@@ -3,13 +3,19 @@ Command-Line Interface for the ridership analysis
 """
 import click
 
-from .main import match_septa_ridership_with_osm, analyze_ridership
+from .main import match_septa_ridership_with_osm, match_njt_ridership_with_osm, analyze_ridership
 
 
 @click.command()
 def ridership_match_osm_w_septa():
-    """Match ridership segments with OSM features"""
+    """Match SEPTA ridership segments with OSM features"""
     match_septa_ridership_with_osm()
+
+
+@click.command()
+def ridership_match_osm_w_njt():
+    """Match SEPTA ridership segments with OSM features"""
+    match_njt_ridership_with_osm()
 
 
 @click.command()
