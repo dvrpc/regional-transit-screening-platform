8392ec69941a61d1540b5c520cdd15de790b31d7
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2021-02-12 15:49:27 -0500

    update to use pg_data_etl.Database()

--- regional_transit_screening_platform/step_02_average_speed/main.py
+++ regional_transit_screening_platform/step_02_average_speed/main.py
@@ -8,8 +8,7 @@ from regional_transit_screening_platform import db, match_features_with_osm
 
 
 def match_speed_features_with_osm(
-    # speed_table: str = "speed.rtsp_input_speed",
-    speed_table: str = "test_speed",
+    speed_table: str = "speed.rtsp_input_speed",
 ):
     """
     Identify OSM features that match each speed segment for surface transit
@@ -19,11 +18,13 @@ def match_speed_features_with_osm(
 
 
 def analyze_speed(
-    speed_table: str = "rtsp_input_speed",
-    match_table: str = "osm_matched_rtsp_input_speed",
+    speed_table: str = "speed.rtsp_input_speed",
+    match_table: str = "osm_matched_speed_rtsp_input_speed",
 ):
 
     # Make a table of all OSM features that matched a speed feature
+    new_tbl = "osm_speed"
+
     query = f"""
         select *
         from  osm_edges
@@ -32,17 +33,17 @@ def analyze_speed(
             from {match_table}
         )
     """
-    db.make_geotable_from_query(query, "osm_speed", "LINESTRING", 26918)
+    db.make_geotable_from_query(query, new_tbl, "LINESTRING", 26918)
 
     # Add columns to the OSM edge layer called 'avgspeed' and 'num_obs'
-    make_speed_col = """
-        alter table osm_speed drop column if exists avgspeed;
-        alter table osm_speed add column avgspeed float;
+    make_speed_col = f"""
+        alter table {new_tbl} drop column if exists avgspeed;
+        alter table {new_tbl} add column avgspeed float;
 
-        alter table osm_speed drop column if exists num_obs;
-        alter table osm_speed add column num_obs float;
+        alter table {new_tbl} drop column if exists num_obs;
+        alter table {new_tbl} add column num_obs float;
     """
-    db.execute(make_speed_col)
+    db.execute_via_psycopg2(make_speed_col)
 
     # Analyze each speed feature
     query = f"select distinct osmuuid from {match_table}"
@@ -59,16 +60,16 @@ def analyze_speed(
                           from {match_table} m
                           where m.osmuuid = '{osmuuid}')
         """
-        result = db.query_as_list(speed_query)
+        result = db.query_via_psycopg2(speed_query)
         avgspeed, num_obs = result[0]
 
         update_query = f"""
-            UPDATE osm_speed
+            UPDATE {new_tbl}
             SET avgspeed = {avgspeed},
                 num_obs = {num_obs}
             WHERE osmuuid = '{osmuuid}';
         """
-        db.execute(update_query)
+        db.execute_via_psycopg2(update_query)
 
     # Draw a line from the centroid of the speed feature to the OSM centroid
     qaqc = f"""
@@ -81,7 +82,7 @@ def analyze_speed(
             ) as geom
         from {match_table} m
         left join
-            osm_speed s
+            {new_tbl} s
             on s.osmuuid = m.osmuuid::uuid
         left join
             {speed_table} f
@@ -90,11 +91,11 @@ def analyze_speed(
     db.make_geotable_from_query(qaqc, "osm_speed_qaqc", "LINESTRING", 26918)
 
     # Add a length column to the QAQC table
-    length_col = """
-        alter table osm_speed_qaqc add column feat_len float;
-        update osm_speed_qaqc set feat_len = st_length(geom);
+    length_col = f"""
+        alter table {new_tbl}_qaqc add column feat_len float;
+        update {new_tbl}_qaqc set feat_len = st_length(geom);
     """
-    db.execute(length_col)
+    db.execute_via_psycopg2(length_col)
 
 
 if __name__ == "__main__":
